549683c097ef3b7ccba6d752f30a8ec8c8376b2a
Liubov-iam <liuboviam16@gmail.com>
2024-05-03 14:50:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import ContactList from "./components/contactList/ContactList";
+import SearchBox from "./components/searchBox/SearchBox";
+import ContactForm from "./components/contactForm/ContactForm";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./redux/contactsOps";
+import "./App.css";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </>
+  );
+}
+
+export default App;
